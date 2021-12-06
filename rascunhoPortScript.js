93e8b716970fb58e5af3bf6bb906f311e878ea64
jlczrffc <jlczrcarvalho@gmail.com>
2021-12-06 16:36:43 -0300

    refatorando codigo... erro em exibir chaves corrigido...

--- rascunhoPortScript.js
+++ rascunhoPortScript.js
@@ -1,4 +1,4 @@
-let archive = process.argv[2]
+	let archive = process.argv[2]
 let option = process.argv[3]
 
 var fs = require('fs');
@@ -10,128 +10,121 @@ fs.readFile(archive, 'utf-8', function(err, data){
 	let partial = ''
 	let seek = 0
 	try{
-		commands = data.split(/\n?\s/)
+		commands = data.split(/\n/)
 		commands.forEach(function(command){
 			while(command.length > 0 ){
 				if (command.match(/\b(para)\b/i)){
 					partial = command.substring(0,command.indexOf('para'))
-					partial = partial + command.substring(command.indexOf('para')+4,command.length)
+					partial = partial + 'for' + command.substring(command.indexOf('para')+4,command.length)
 					command = partial
-					result = result + 'for'
-				}else if (command.match(/\b(permita)\b/i)){
+				} if (command.match(/\b(permita)\b/i)){
 					partial = command.substring(0,command.indexOf('permita'))
-					partial = partial + command.substring(command.indexOf('permita')+7,command.length)
+					partial = partial + 'let ' + command.substring(command.indexOf('permita')+7,command.length)
 					command = partial
-					result = result + 'let '  
-				}else if (command.match(/\b(variavel)\b/i)){
+				} if (command.match(/\b(variavel)\b/i)){
 					partial = command.substring(0,command.indexOf('variavel'))
-					partial = partial + command.substring(command.indexOf('variavel')+7,command.length)
+					partial = partial + 'var ' + command.substring(command.indexOf('variavel')+8,command.length)
 					command = partial
-					result = result + 'var '  
-				}else if (command.match(/\b(se)\b/i)){
+				} if (command.match(/\b(se)\b/i)){
 					partial = command.substring(0,command.indexOf('se'))
-					partial = partial + command.substring(command.indexOf('se')+2,command.length)
+					partial = partial + 'if' + command.substring(command.indexOf('se')+2,command.length)
 					command = partial
-					result = result + 'if'  
-				}else if (command.match(/\b(constante)\b/i)){
+				} if (command.match(/\b(constante)\b/i)){
 					partial = command.substring(0,command.indexOf('constante'))
-					partial = partial + command.substring(command.indexOf('constante')+9,command.length)
+					partial = partial + 'const ' + command.substring(command.indexOf('constante')+9,command.length)
 					command = partial
-					result = result + 'const '  
-				}else if (command.match(/\b(imprima)\b/i)){
+				} if (command.match(/\b(imprima)\b/i)){
 					partial = command.substring(0,command.indexOf('imprima'))
-					partial = partial + command.substring(command.indexOf('imprima')+7,command.length)
+					partial = partial + 'console.log' + command.substring(command.indexOf('imprima')+7,command.length)
 					command = partial
-					result = result + 'console.log'  
-				}else if (command.match(/\b(verdadeiro)\b/i)){
+				} if (command.match(/\b(verdadeiro)\b/i)){
 					partial = command.substring(0,command.indexOf('verdadeiro'))
-					partial = partial + command.substring(command.indexOf('verdadeiro')+10,command.length)
+					partial = partial + 'true' + command.substring(command.indexOf('verdadeiro')+10,command.length)
 					command = partial
-					result = result + 'true'  
-				}else if (command.match(/\b(falso)\b/i)){
+					console.log('#############################' + command +'#######################')
+				} if (command.match(/\b(falso)\b/i)){
 					partial = command.substring(0,command.indexOf('falso'))
-					partial = partial + command.substring(command.indexOf('falso')+5,command.length)
+					partial = partial + 'false' + command.substring(command.indexOf('falso')+5,command.length)
 					command = partial
-					result = result + 'false'
-				}else if (command.match(/\b(enquanto)\b/i)){
+				} if (command.match(/\b(enquanto)\b/i)){
 					if (command.match(/([}])/)){
 						partial = command.substring(0,command.indexOf('}enquanto'))
-						partial = partial + command.substring(command.indexOf('}enquanto')+9,command.length)
+						partial = partial + '}while' + command.substring(command.indexOf('}enquanto')+9,command.length)
 						command = partial
-						result = result + '}while'
 					}else{
 						partial = command.substring(0,command.indexOf('enquanto'))
-						partial = partial + command.substring(command.indexOf('enquanto')+8,command.length)
+						partial = partial + 'while' + command.substring(command.indexOf('enquanto')+8,command.length)
 						command = partial
-						result = result + 'while'
 					}  
-				}else if (command.match(/\b(faca)\b/i)){
+				} if (command.match(/\b(faca)\b/i)){
 					partial = command.substring(0,command.indexOf('faca'))
-					partial = partial + command.substring(command.indexOf('faca')+4,command.length)
+					partial = partial + 'do' + command.substring(command.indexOf('faca')+4,command.length)
 					command = partial
-					result = result + 'do'  
-				}else if (command.match(/\b(funcao)\b/i)){
+				} if (command.match(/\b(funcao)\b/i)){
 					partial = command.substring(0,command.indexOf('funcao'))
-					partial = partial + command.substring(command.indexOf('funcao')+6,command.length)
+					partial = partial + 'function ' + command.substring(command.indexOf('funcao')+6,command.length)
 					command = partial
-					result = result + 'function '  
-				}else if (command.match(/\b(retorne)\b/i)){
+				} if (command.match(/\b(retorne)\b/i)){
 					partial = command.substring(0,command.indexOf('retorne'))
-					partial = partial + command.substring(command.indexOf('retorne')+7,command.length)
+					partial = partial + 'return ' + command.substring(command.indexOf('retorne')+7,command.length)
 					command = partial
-					result = result + 'return '  
-				}else if (command.match(/\b(troque)\b/i)){
+				} if (command.match(/\b(troque)\b/i)){
 					partial = command.substring(0,command.indexOf('troque'))
-					partial = partial + command.substring(command.indexOf('troque')+6,command.length)
+					partial = partial + 'switch' + command.substring(command.indexOf('troque')+6,command.length)
 					command = partial
-					result = result + 'switch'  
-				}else if (command.match(/\b(caso)\b/i)){
+				} if (command.match(/\b(caso)\b/i)){
 					partial = command.substring(0,command.indexOf('caso'))
-					partial = partial + command.substring(command.indexOf('caso')+4,command.length)
+					partial = partial + 'case' + command.substring(command.indexOf('caso')+4,command.length)
 					command = partial
-					result = result + 'case'  
-				}else if (command.match(/\b(saia)\b/i)){
+				} if (command.match(/\b(saia)\b/i)){
 					partial = command.substring(0,command.indexOf('saia'))
-					partial = partial + command.substring(command.indexOf('saia')+4,command.length)
+					partial = partial + 'break' + command.substring(command.indexOf('saia')+4,command.length)
 					command = partial
-					result = result + 'break'  
-				}else if (command.match(/\b(predefina)\b/i)){
+				} if (command.match(/\b(predefina)\b/i)){
 					partial = command.substring(0,command.indexOf('predefina'))
-					partial = partial + command.substring(command.indexOf('predefina')+9,command.length)
+					partial = partial + 'default' + command.substring(command.indexOf('predefina')+9,command.length)
 					command = partial
-					result = result + 'default'  
+				} if (command.match(/\b(paraCada)\b/i)){
+					partial = command.substring(0,command.indexOf('paraCada'))
+					partial = partial + 'forEach' + command.substring(command.indexOf('paraCada')+8,command.length)
+					command = partial
+				}
+				if (command.match(/([a-z]+)/)){
+					result = result + command
+					command = ''  
 				}else if (command.match(/([0-9]+)/)){
 					result = result + command
 					command = ''  
-				}else if (command.match(/([a-z]+)/)){
-					result = result + command + ' '
-					command = ''  
-				}else if (command.match(/[/'"+-{}()*&#@%]/)){
+				}else if (command.match(/[/'"+-{}()*&#@%;]/)){
 					result = result + command
 					command = '' 
 				}
 			}
 		})
-		for(let l = 0; l < result.length; l++){
-			let letter = result.substring(l,l+1)
-			if(result.substring(l,l+3) === 'for'){
-				while(seek < 2){
-					if(result.substring(l,l+1) !== ')'){
-						l++
-					}else{
-						seek++
-					}
-				}
-				seek = 0
-			}
-			if (letter === '{'){
-				result=result.substring(0,l) + '{\n' +result.substring(l+1,result.length)
-			}
-			if (letter === ';'){
-				result=result.substring(0,l) + ';\n' +result.substring(l+1,result.length)
-			}
-		}
 		if (option === '-code'){ 
+			for(let l = 0; l < result.length; l++){
+				let letter = result.substring(l,l+1)
+				if(result.substring(l,l+3) === 'for'){
+					while(seek < 2){
+						if(result.substring(l,l+1) !== ')'){
+							l++
+						}else{
+							seek++
+						}
+					}
+					seek = 0
+				}
+				if (letter === '{'){
+					result=result.substring(0,l) + '{\n' +result.substring(l+1,result.length)
+				}
+				if (letter === ';'){
+					result=result.substring(0,l) + ';\n' +result.substring(l+1,result.length)
+				}
+				if (letter === '}'){
+					console.log('******************* '+ l)
+					result=result.substring(0,l-1) + '\n' +result.substring(l,result.length)
+				}
+			}
 			console.log(result)
 		}
 		eval(result);
